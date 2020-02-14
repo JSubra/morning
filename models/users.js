6be2586d67f6d1755fcec5afc53cc39be02d3a36
Juliette Subra <juliette.subra@gmail.com>
2020-02-14 11:19:25 +0100

    bdd wishlist

--- models/users.js
+++ models/users.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose')
 
+const wishlistSchema = mongoose.Schema({
+    id: String,
+    name: String,
+    description: String,
+    url : String,
+    language : String
+
+})
+
 const userSchema = mongoose.Schema({
     username: String,
     email: String,
     password: String,
     token: String,
     salt: String, 
+    wishlist: wishlistSchema
 })
 
 const userModel = mongoose.model('users', userSchema)
